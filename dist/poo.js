27706a4c7c73a671df5bf31d9332cce66ee27533
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-09-07 23:30:50 +0200

    Herencia en TS

--- dist/poo.js
+++ dist/poo.js
@@ -1,24 +1,39 @@
 "use strict";
 class Personaje {
-    constructor(id, name, nivel, hp) {
+    constructor(id, name, nivel, _hp) {
         this.id = id;
         this.name = name;
         this.nivel = nivel;
-        this.hp = hp;
+        this._hp = _hp;
     }
     subirNivel() {
         this.nivel += 1;
         return this.nivel;
     }
     cambiarHp(cantidad) {
-        this.hp += cantidad;
-        return this.hp;
+        this._hp += cantidad;
+        return this._hp;
+    }
+    get hp() {
+        return this._hp;
+    }
+    static agregarPj() {
+        Personaje._equipo++;
+    }
+    static getEquipo() {
+        return Personaje._equipo;
     }
 }
+Personaje._equipo = 0;
 const personaje = new Personaje(1, "Carrot", 1, 100);
+Personaje.agregarPj();
+const personaje2 = new Personaje(2, "Curry", 1, 120);
+Personaje.agregarPj();
+console.log("Cantidad de Personajes: ", Personaje.getEquipo());
 console.log(personaje);
 personaje.cambiarHp(-10);
+console.log(personaje, personaje2);
 if (personaje instanceof Personaje) {
-    console.log(personaje);
+    console.log(personaje.hp);
 }
 //# sourceMappingURL=poo.js.map
--- src/poo.ts
+++ src/poo.ts
@@ -1,13 +1,17 @@
 class Personaje {
-    id: number
-    name: string
-    nivel: number
-    private _hp: number // con la palabra reservada "private" gestionamos control de acceso a propiedades
-    constructor (id: number, name: string, nivel: number, hp: number) {
-        this.id = id
-        this.name = name
-        this.nivel = nivel
-        this._hp = hp
+    // readonly id: number // solo lectura.
+    // name: string
+    // nivel: number
+    // private _hp: number // con la palabra reservada "private" gestionamos control de acceso a propiedades.
+    // como "profesion" no tiene valor inicial, queda fuera del constructor
+    profesion?: string // esta propiedad es opcional, no hay que definirla en la creacion del objeto.
+    private static _equipo: number = 0
+    constructor (
+        public readonly id: number, 
+        public name: string, 
+        public nivel: number, 
+        private _hp: number
+    ) {
     }
 
     subirNivel(): number {
@@ -19,16 +23,43 @@ class Personaje {
         this._hp += cantidad
         return this._hp
     }
+
+    get hp(): number {
+        return this._hp
+    }
+
+    // los "setters" se usan mas a menudo para validacion de datos, no para este tipo de funcion,
+    // siempre es mejor hacer un metodo para cambiar una propiedad privada
+    // set hp(cantidad: number) {
+    //     this._hp += cantidad
+    // } 
+
+    static agregarPj(): void {
+        Personaje._equipo++
+    }
+    
+    static getEquipo(): number {
+        return Personaje._equipo
+    }
 }
 
 const personaje = new Personaje(1, "Carrot", 1, 100)
+Personaje.agregarPj()
+const personaje2 = new Personaje(2, "Curry", 1, 120)
+Personaje.agregarPj()
+console.log("Cantidad de Personajes: ",Personaje.getEquipo())
+
+
 
 console.log(personaje)
 personaje.cambiarHp(-10)
 // personaje.subirNivel()
 // console.log(personaje)
 
+console.log(personaje, personaje2)
+
 
 if (personaje instanceof Personaje) {
-    console.log(personaje)
+    console.log(personaje.hp)
 }
+
